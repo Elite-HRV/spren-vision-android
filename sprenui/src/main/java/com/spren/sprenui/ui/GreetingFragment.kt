e87be3cfc16c5fbbc39a84315ab79c9359113157
kevin-spren <kevin@elitehrv.com>
2022-09-29 12:22:13 -0600

    Removing character

--- sprenui/src/main/java/com/spren/sprenui/ui/GreetingFragment.kt
+++ sprenui/src/main/java/com/spren/sprenui/ui/GreetingFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.spren.sprenui.R
-=import com.spren.sprenui.SprenUI
+import com.spren.sprenui.SprenUI
 import com.spren.sprenui.databinding.FragmentGreetingBinding
 
 class GreetingFragment : Fragment() {
